42a43dfda0cb218a4c00cb7f63623891bc31d2b3
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2024-03-14 01:20:08 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
-
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
-  console.log("");
-
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+  // Iterate through the array
+  for (let i = 0; i < array.length; i++) {
+    // Iterate through the array again starting from the next element
+    for (let j = i + 1; j < array.length; j++) {
+      // Check if the sum of the current pair of numbers equals the target
+      if (array[i] + array[j] === target) {
+        return true; // If found, return true
+      }
+    }
+  }
+  // If no pair is found, return false
+  return false;
 }
 
 module.exports = hasTargetSum;
